dc564e903255dba42ef7efea4ecb07f1e2bee2fb
Madhav-Paudel <madhav.paudel40400@gmail.com>
2025-01-14 17:16:34 +0545

    in this portion i make a number guessing game in python

--- exc10.py
+++ exc10.py
@@ -3,8 +3,15 @@ import random
 
 # number=random.randint(1,5)
 # print(number)
-low=1
-high=100
-number=random.randint(low,high)
-print(number)
+# low=1
+# high=100
+# number=random.randint(low,high)
+# print(number)
 
+# for getting the floating number from random module 
+# number=random.random()
+# print(number)
+# getting random thing form tuple
+# options=("rock","paper","scissors")
+# option=random.choice(options)
+# print(option)
---
+++ project/numberguss.py
@@ -0,0 +1,33 @@
+# in this program we will create number guessing game 
+import random
+lowest=1
+highest=100
+# this generate random number
+answer=random.randint(lowest,highest)
+# for tracking the number of guess 
+T_guess=0
+# print(answer)
+guess_incorrect=True
+print("This is number guessing game")
+print(f"Please enter the number between {lowest} and {highest}")
+while guess_incorrect:
+    value=input("Number:")
+    print(f"Your guess amount {T_guess+1}")
+
+    if value.isdigit():
+            value=int(value)
+            T_guess+=1
+            if value== answer:
+             guess_incorrect=False
+            elif value <answer:
+                print("Your answer is short by some point")
+            else:
+                print("Your answer is large by some point") 
+            
+    else:
+        print("Invalid input you enter string ")
+        print(f"Please enter the number between {lowest} and {highest}")
+
+print(f" You guess is correct the value is {answer}")
+print(f"total guess you required is {T_guess}")
+
