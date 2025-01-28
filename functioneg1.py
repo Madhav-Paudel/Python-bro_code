9e52a9bd9f40bc879d99ef3438a9853dd8c2956d
Madhav Paudel <madhav.paudel40400@gmail.com>
2025-01-28 20:18:02 +0545

    there is some exercise what i have done

---
+++ functioneg1.py
@@ -0,0 +1,7 @@
+def happy_birthday():
+    print("hello i am Madhav and this is my birthday month")
+    print("i love ai and ml but i am quite lazy ")
+    print()
+
+happy_birthday()
+happy_birthday()
